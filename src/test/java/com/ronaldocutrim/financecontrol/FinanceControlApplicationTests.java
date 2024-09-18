f5eab92f0f5869024609ea6d9ba2768593bd10a1
Ronaldo Cutrim <ronaldocutrim@gmail.com>
2024-09-18 20:27:29 -0300

    initial commit

---
+++ src/test/java/com/ronaldocutrim/financecontrol/FinanceControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronaldocutrim.financecontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinanceControlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
